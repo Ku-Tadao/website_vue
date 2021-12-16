9435f98aec187b8258b9c18fd35371063b52a0a7
Kubilay Karabulut <PS215264@summacollege.nl>
2021-12-16 14:51:52 +0100

    Version update, added links to correct routes.

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/nieuws">Nieuws</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/news">Nieuws</router-link> |
+      <router-link to="/images">Afbeeldingen</router-link> |
+      <router-link to="/about">Over ons</router-link> |
+      <router-link to="/contact">Contact</router-link>
     </div>
     <router-view/>
   </div>
