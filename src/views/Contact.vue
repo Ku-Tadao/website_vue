a55de785e45343548b347d2467e3f0863d99f356
Kubilay Karabulut <PS215264@summacollege.nl>
2021-12-16 14:49:48 +0100

    Added Rquired Views

---
+++ src/views/Contact.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="contact">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>This is an contact page</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'contact',
+  components: {
+    
+  }
+}
+</script>
---
+++ src/views/Images.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="images">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>This is an images page</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Images',
+  components: {
+    
+  }
+}
+</script>
